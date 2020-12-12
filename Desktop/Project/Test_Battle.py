14434e3735637b84d0181e1310ed2932ac8a7592
birdengin <ChayakornGH@gmail.com>
2020-12-12 21:03:59 +0700

    Update 09:00PM 12/12/2020

--- Desktop/Project/Test_Battle.py
+++ Desktop/Project/Test_Battle.py
@@ -1,4 +1,5 @@
 #Test
+import random
 Pokemon =   {
             "Bulbasaur":
                 {'Hp':45,'Atk':49,'Def':49,'Sp.Atk':65,'Sp.Def':65,'Speed':45,
@@ -124,100 +125,6 @@ Pokemon =   {
                 }}
 }
 #==========================================================================================================
- def MainBattle(p1,p2):
-    p1chp = Hp(p1)
-    p2chp = Hp(p2)
-    Priority = [] #ลำดับการตี
-    Priority.append(Pokemon[p1]['Speed'],Pokemon[p2]['Speed'])
-    if p1chp !=0 or p2chp != 0:
-        if Priority[0] == Pokemon[p1]['Speed']:
-            Battle(p1,p2)
-        else:
-            Battle(p2,p1)
-    else:
-        if p1chp !=0:
-            print(p1,'Won the battle!!')
-        else:
-            print(p2,'Won the battle!!')
-
-##ทำที่ใส่นิสัยแบบใส่โปเกมอน
-##PokemonP1 =[p1,movech1]
-##print(PokemonP1)
-def Priority(use1,use2):
-    p = []
-    p5p = ['Helping Hand']
-    p4p = ['Baneful Bunker','Detect','Endure',"King's Shield",'Magic Coat','Protect','Spiky Shield','Snatch']
-    p3p = ['Crafty Shield','Fake Out','Quick Guard','Wide Guard']
-    p2p = ['Ally Switch','Extreme Speed','Feint','First Impression','Follow Me','Rage Powder','Zippy Zap']
-    p1p = ['Accelerock','Aqua Jet','Baby-Doll Eyes','Bide','Bullet Punch','Ice Shard','Ion Deluge','Mach Punch','Powder','Quick Attack','Shadow Sneak','Sucker Punch','Vacuum Wave','Water Shuriken']
-    p1m = ['Vital Throw']
-    p3m = ['Focus Punch']
-    p4m = ['Avalanche','Revenge']
-    p5m = ['Counter','Mirror Coat']
-    p6m = ['Circle Throw','Dragon Tail','Roar','Whirlwind']
-    p7m = ['Trick Room']
-    prip = [p5p,p4p,p3p,p2p,p1p] #Priority Plus
-    prim = [p1m,p3m,p4m,p5m,p6m,p7m] #Priority Minus
-    pri = [p5p,p4p,p3p,p2p,p1p,p1m,p3m,p4m,p5m,p6m,p7m]
-    for i in pri:
-        if i == p5p:
-            j = +5
-        elif i == p4p:
-            j = +4
-        elif i == p3p:
-            j = +3
-        elif i == p2p:
-            j = +2
-        elif i == p1p:
-            j = +1
-        elif i == p1m:
-            j = -1
-        elif i == p3m:
-            j = -3
-        elif i == p4m:
-            j = -4
-        elif i == p5m:
-            j = -5
-        elif i == p6m:
-            j = -6
-        elif i == p7m:
-            j = -7
-        else :
-            j = 0
-        if use1 in i:
-            use1 = {use1:j}
-        if use2 in i:
-            use2 = {use2:j}
-    if use1[use1] == use2[use2]:
-        return
-
-def Status(p1,p2):
-#=================================================================
-def Battle(PokemonP1,PokemonP2): ## Phrase ต่อสู้
-    for i in PokemonP1[1]:
-        print(i)
-    use1 = input("Choose your move!! :")
-    for i in PokemonP2[1]:
-        print(i)
-    use2 = input("Choose your opponent move!! :")
-    dmg1 = Type(p1,use1,p2)
-    dmg2 = Type(p2,use2,p1)
-
-def Mod(p1,usage_move):
-    if Pokemon[p1]['Moves'][usage_move] == ''
-#=========================================================================================================
-def Type(p1,usage_move,p2): ##Type Calculator
-    p1movetype = Pokemon[p1]['Moves'][usage_move]['Type']
-    if p1movetype in Pokemon[p2]['Weakness']:#usage move == ชื่อท่า
-        dmg = int(Pokemon[p2]['Weakness'][p1movetype])*int(Pokemon[p1]['Moves'][usage_move]['pwr'])
-    elif p1movetype in Pokemon[p2]['Resistant']:
-        dmg = int(Pokemon[p2]['Resistant'][p1movetype])*int(Pokemon[p1]['Moves'][usage_move]['pwr'])
-    elif p1movetype in Pokemon[p2]['Immune']:
-        dmg = int(Pokemon[p2]['Immune'][p1movetype])*int(Pokemon[p1]['Moves'][usage_move]['pwr'])
-    else:
-        dmg = int(Pokemon[p1]['Moves'][usage_move]['pwr'])
-    return dmg
-#==========================================================================================================
 def Hp(p1): #Real Hp
     RHP = int(((((2*Pokemon[p1]['Hp'])+31)/100)*50)+50+10)
     ##RHP = int(((((2*Pokemon[p1]['Hp'])+31+Pokemon[p1]['EVs'])/100)*50)+50+10)
@@ -252,6 +159,195 @@ def Realstats(p1): ## stats จริงของมอน
     RS.append(Speed(p1))
     return RS
 #==========================================================================================================
+##ทำที่ใส่นิสัยแบบใส่โปเกมอน
+##PokemonP1 =[p1,movech1]
+##print(PokemonP1)
+def Priority(p1,p2):
+    use1 = p1[2]
+    use2 = p2[2]
+    p = []
+    p5p = ['Helping Hand']
+    p4p = ['Baneful Bunker','Detect','Endure',"King's Shield",'Magic Coat','Protect','Spiky Shield','Snatch']
+    p3p = ['Crafty Shield','Fake Out','Quick Guard','Wide Guard']
+    p2p = ['Ally Switch','Extreme Speed','Feint','First Impression','Follow Me','Rage Powder','Zippy Zap']
+    p1p = ['Accelerock','Aqua Jet','Baby-Doll Eyes','Bide','Bullet Punch','Ice Shard','Ion Deluge','Mach Punch','Powder','Quick Attack','Shadow Sneak','Sucker Punch','Vacuum Wave','Water Shuriken']
+    p1m = ['Vital Throw']
+    p3m = ['Focus Punch']
+    p4m = ['Avalanche','Revenge']
+    p5m = ['Counter','Mirror Coat']
+    p6m = ['Circle Throw','Dragon Tail','Roar','Whirlwind']
+    p7m = ['Trick Room']
+    prip = [p5p,p4p,p3p,p2p,p1p] #Priority Plus
+    prim = [p1m,p3m,p4m,p5m,p6m,p7m] #Priority Minus
+    pri = [p5p,p4p,p3p,p2p,p1p,p1m,p3m,p4m,p5m,p6m,p7m]
+    using1 = use1
+    using2 = use2
+    for i in pri:
+        if i == p5p:
+            j = +5
+        elif i == p4p:
+            j = +4
+        elif i == p3p:
+            j = +3
+        elif i == p2p:
+            j = +2
+        elif i == p1p:
+            j = +1
+        elif i == p1m:
+            j = -1
+        elif i == p3m:
+            j = -3
+        elif i == p4m:
+            j = -4
+        elif i == p5m:
+            j = -5
+        elif i == p6m:
+            j = -6
+        elif i == p7m:
+            j = -7
+        else:
+            j=0
+        if use1 in i:
+            use1 = [use1,j]
+            break
+        else:
+            use1 = [use1,0]
+            break
+    for i in pri:
+        if i == p5p:
+            j = +5
+        elif i == p4p:
+            j = +4
+        elif i == p3p:
+            j = +3
+        elif i == p2p:
+            j = +2
+        elif i == p1p:
+            j = +1
+        elif i == p1m:
+            j = -1
+        elif i == p3m:
+            j = -3
+        elif i == p4m:
+            j = -4
+        elif i == p5m:
+            j = -5
+        elif i == p6m:
+            j = -6
+        elif i == p7m:
+            j = -7
+        else:
+            j=0
+        if use2 in i:
+            use2 = [use2,j]
+            break
+        else:
+            use2 = [use2,0]
+            break
+    if use1[1] == use2[1]:
+        if Pokemon[p1[0]]['Speed'] > Pokemon[p2[0]]['Speed']:
+            p.append(using1)
+            p.append(using2)
+            return p
+        elif Pokemon[p1[0]]['Speed'] < Pokemon[p2[0]]['Speed']:
+            p.append(using2)
+            p.append(using1)
+            return p
+    elif use1[1] > use2[1]:
+        p.append(using1)
+        p.append(using2)
+        return p
+    else: #use 2 > use 1
+        p.append(using2)
+        p.append(using1)
+        return p
+#====================================================================================================================================       
+""" def Status(p1,p2):
+    if  """
+#=====================================================================================================================================
+def Battle(p1,p2): ## Phrase ต่อสู้
+    for i in p1[1]:
+        print(i)
+    use1 = input("Choose your move!! :")
+    for i in p2[1]:
+        print(i)
+    use2 = input("Choose your opponent move!! :")
+    p1.append(use1)
+    p2.append(use2)
+    return [p1,p2]
+PokemonP1 = ['Charizard',['Flare Blitz','Inferno','Fire Spin','Air Slash']]
+PokemonP2 = ['Venusaur',['Solar Beam','Poison Powder','Sleep Powder','Take Down']]
+""" p = Battle(PokemonP1,PokemonP2)
+move1,move2 = p[0],p[1]
+Priority = Priority(move1,move2) """
+def movec(move):#Critical calculator 
+    if move != 'Razor Leaf' and move != 'Crabhammer' and move != 'Razor Wind' and move!='Shadow Claw' and move!='Slash' and move!='Stone Edge' and move!='Night Slash':
+        p =random.randint(0,23)
+        if p==1 :
+            return 1.5
+        else:
+            return 1
+    else:
+        p = random.randint(0,7)
+        if p ==1:
+            return 1.5
+        else:
+            return 1
+def Damaging(Move,PokemonP1,PokemonP2): #PokemonP1 = ['Charizard',[Moves],[usage_move]]
+    randomfactor = random.uniform(0.85,1.00)
+    critical = movec(Move)
+    print(Pokemon[PokemonP1[0]]['Moves'][Move]['Type'])
+    print(Pokemon[PokemonP1[0]]['Type'])
+    if Move in PokemonP1:
+        if Pokemon[PokemonP1[0]]['Moves'][Move]['Type'] in Pokemon[PokemonP1[0]]['Type']:
+            STAB = 1.5
+        else:
+            STAB = 1.0
+        if 'Burn' in PokemonP1:
+            Burn = 0.5
+        else:
+            Burn = 1
+        Modifier = critical*randomfactor*STAB*Burn
+        damage = (((((((((2*50)/5)+2)*Type(Pokemon[PokemonP1[0]],Move,Pokemon[PokemonP2[0]]))*Atk(PokemonP1[0]))/Def(PokemonP2[0]))+((100*Type(Pokemon[PokemonP1[0]],Move,Pokemon[PokemonP2[0]]))/Pokemon[PokemonP1[0]]['Moves'][Move]['pwr']))/50))*Modifier
+        ##damage = ((((((((2*50)/5)+2)*power)*Atk(p1))/Def(p2))/50)+2)*Modifier
+        print(dmg)
+    else :
+        damage = ((((((((2*50)/5)+2)*power)*Atk(PokemonP2[0]))/Def(PokemonP1[0]))/50)+2)
+#=====================================================================================================================================
+""" 
+def Mod(p1,usage_move):
+    if Pokemon[p1]['Moves'][usage_move] == ''
+"""
+#=====================================================================================================================================
+def Type(p1,usage_move,p2): ##Type Calculator
+    p1movetype = Pokemon[p1]['Moves'][usage_move]['Type']
+    if p1movetype in Pokemon[p2]['Weakness']:#usage move == ชื่อท่า
+        dmg = int(Pokemon[p2]['Weakness'][p1movetype])*int(Pokemon[p1]['Moves'][usage_move]['pwr'])
+    elif p1movetype in Pokemon[p2]['Resistant']:
+        dmg = int(Pokemon[p2]['Resistant'][p1movetype])*int(Pokemon[p1]['Moves'][usage_move]['pwr'])
+    elif p1movetype in Pokemon[p2]['Immune']:
+        dmg = int(Pokemon[p2]['Immune'][p1movetype])*int(Pokemon[p1]['Moves'][usage_move]['pwr'])
+    else:
+        dmg = int(Pokemon[p1]['Moves'][usage_move]['pwr'])
+    return dmg
+
+#==========================================================================================================
+def MainBattle(p1,p2): 
+    p1chp = Hp(p1[0]) #Player 1 Current Hp
+    p2chp = Hp(p2[0]) #Player 2 Current Hp
+    if p1chp !=0 and p2chp !=0:
+        p = Battle(PokemonP1,PokemonP2)
+        move1,move2 = p[0],p[1]
+        Respect = Priority(move1,move2)
+        for i in Respect:
+            Damaging(i,p1,p2)
+    else:
+        if p1chp !=0:
+            print(p1,'Won the battle!!')
+        else:
+            print(p2,'Won the battle!!')
+MainBattle(PokemonP1,PokemonP2)
+#======================================================================================================================
 """ q1 = True
 while q1 == True:
     p1 = input("Choose your Pokemon!! : ")
@@ -313,9 +409,9 @@ while q2 == True:
         break
     else:
         pass
-PokemonP2 =[p2,movech1]
+PokemonP2 =[p2,movech2]
 print(PokemonP2)
-battle.confirm=None
+print(p1,p2)
+battle_confirm=None
 while battle_confirm !='Battle':
-    battle_confirm = input("Press 'Battle' to start the battle!!\n :")
-MainBattle(p1,p2) """
+    battle_confirm = input("Press 'Battle' to start the battle!!\n :") """
